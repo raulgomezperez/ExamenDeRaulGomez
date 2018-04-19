3d5d0d3f699545d5e1f67faf503b1f59891ca082
Raúl Gómez <37397597+raulgomezperez@users.noreply.github.com>
2018-04-19 09:25:44 +0200

    Ejercicio 3 realizado

--- ExamenDeRaulGomez/src/org/rgp/ExamenDeRaulGomez.java
+++ ExamenDeRaulGomez/src/org/rgp/ExamenDeRaulGomez.java
@@ -5,5 +5,6 @@ public class ExamenDeRaulGomez {
 	public static void main(String[] args) {
 		System.out.println("La fecha de hoy es: 19/14/2018");
 		//Ejercicio 2 realizado
+		//Ejercicio 3 realizado
 	}
 }
